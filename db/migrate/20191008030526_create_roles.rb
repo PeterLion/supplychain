9839d9dc9f80b886b652ec01c3baad1109479c49
Pierre Celestin NSHIMYUMUREMYI <nshimiyepeter@gmail.com>
2019-10-09 21:10:38 -0600

    edited the role migration

--- db/migrate/20191008030526_create_roles.rb
+++ db/migrate/20191008030526_create_roles.rb
@@ -1,8 +1,8 @@
 class Role < ActiveRecord::Migration[6.0]
   def change
     create_table :roles do |t|
-      t.string :name,
-      t.text :description,
+      t.string :name
+      t.text :description
 
       t.timestamps
     end
